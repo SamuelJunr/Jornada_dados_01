e4191d52318ef8a9036f42d00ae8fbce02e2232c
SamuelJunr <samuelgomespj@outlook.com>
2024-06-15 21:37:50 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,20 @@
+print ("jornada de dados")
+
+# Variaveis
+idade = 36
+nome = "Samuel"
+
+# Print 
+print(idade)
+
+# Soma
+nova_idade = idade + 1
+print (nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+print (lista)
+
+# Metodos
+Lista_invertida = lista.reverse()
+print (Lista_invertida)
